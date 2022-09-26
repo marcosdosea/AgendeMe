af25142816b7c0371deed0a8a3c14fa6e9af1036
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-09-26 11:22:15 -0300

    Adicionado classes controll, viewmodel e profile

---
+++ Codigo/AgendeMeWeb/Controllers/AreaDeServicoController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendeMeWeb.Controllers
+{
+    public class AreaDeServicoController : Controller
+    {
+        // GET: AreaDeServicoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AreaDeServicoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AreaDeServicoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AreaDeServicoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AreaDeServicoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AreaDeServicoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AreaDeServicoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AreaDeServicoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Codigo/AgendeMeWeb/Models/AreaDeServicoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AgendeMeWeb.Models
+{
+    public class AreaDeServicoViewModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int IdPrefeitura { get; set; }
+        public string? Icone { get; set; }
+    }
+}
