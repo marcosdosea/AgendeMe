339e2933a308f5c4590a3a615c78483122b99cca
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-09-26 11:06:07 -0300

    Adicionado classe de servico AreaDeServicoService.cs

---
+++ Codigo/Service/AreaDeServicoService.cs
@@ -0,0 +1,63 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+    public class AreaDeServicoService : IAreaDeServicoService
+    {
+        private readonly AgendeMeContext _context;
+
+        public AreaDeServicoService(AgendeMeContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Cria uma area de servico no banco de dados
+        /// </summary>
+        /// <param name="areaDeServico">Dados da area de servico</param>
+        /// <returns>Id da area de servico criada</returns>
+        public int Create(Areadeservico areaDeServico)
+        {
+            _context.Add(areaDeServico);
+            _context.SaveChanges();
+            return areaDeServico.Id;
+        }
+        /// <summary>
+        /// Deleta uma area de servico do banco de dados
+        /// </summary>
+        /// <param name="idAreaDeServico">Id da area de servico</param>
+        public void Delete(int idAreaDeServico)
+        {
+            var _areaDeServico = _context.Agendamentos.Find(idAreaDeServico);
+            _context.Remove(_areaDeServico);
+            _context.SaveChanges();
+        }
+        /// <summary>
+        /// Edita uma area de servico do banco de dados
+        /// </summary>
+        /// <param name="areaDeServico">Dados da area de servico</param>
+        public void Edit(Areadeservico areaDeServico)
+        {
+            _context.Update(areaDeServico);
+            _context.SaveChanges();
+        }
+        /// <summary>
+        /// Consulta uma area de servico do banco de dados
+        /// </summary>
+        /// <param name="idAreaDeServico">Id da area de servico</param>
+        /// <returns></returns>
+        public Areadeservico Get(int idAreaDeServico)
+        {
+            return _context.Areadeservicos.Find(idAreaDeServico);
+        }
+        /// <summary>
+        /// Consulta todas as areas de servico do banco de dados
+        /// </summary>
+        /// <returns>Todas as areas de servico</returns>
+        public IEnumerable<Areadeservico> GetAll()
+        {
+            return _context.Areadeservicos.AsNoTracking();
+        }
+    }
+}
