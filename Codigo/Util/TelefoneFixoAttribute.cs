3a795ace7a9e7f29b780049ab199412ad69b2f17
Vinícius Carvalho <vini77carvalho77@gmail.com>
2022-09-26 12:15:15 -0300

    Adição do projeto Util e suas classes com métodos de validação

---
+++ Codigo/Util/TelefoneFixoAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Util
+{
+	/// <summary>
+	/// Validação customizada para CPF
+	/// </summary>
+	public class TelefoneFixoAttribute : ValidationAttribute
+	{
+		/// <summary>
+		/// Construtor
+		/// </summary>
+		public TelefoneFixoAttribute() { }
+
+		/// <summary>
+		/// Validação server
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public override bool IsValid(object? value)
+		{
+			if (value == null || string.IsNullOrEmpty(value.ToString()))
+				return true;
+			var valueNoSpecial = Methods.RemoveSpecialsCaracts((string)value);
+			if (valueNoSpecial.ToString().Length != 10)
+				return false;
+			if (valueNoSpecial.ToString().StartsWith("0"))
+				return false;
+			return true;
+		}
+
+		public string GetErrorMessage() =>
+			$"Telefone Fixo Inválido";
+	}
+}
