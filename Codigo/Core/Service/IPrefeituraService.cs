e323645485949dc07d6b1eed26504e7f410723c8
Carlos Daniel de Jesus <ms_danniel@outlook.com>
2022-09-27 20:20:21 -0300

    Métodos criados na service do core. #252

--- Codigo/Core/Service/IPrefeituraService.cs
+++ Codigo/Core/Service/IPrefeituraService.cs
@@ -2,5 +2,10 @@
 {
     public interface IPrefeituraService
     {
+        int Create(Prefeitura prefeitura);
+        void Edit(Prefeitura prefeitura);
+        void Delete(int idPrefeitura);
+        Prefeitura Get(int idPrefeitura);
+        IEnumerable<Prefeitura> GetAll();
     }
 }
