667584f4d48556eca55e37f3456f16fab682185a
Carlos Daniel de Jesus <ms_danniel@outlook.com>
2022-09-27 22:05:45 -0300

    criadas views para prefeitura. #252

---
+++ Codigo/AgendeMeWeb/Controllers/PrefeituraController.cs
@@ -0,0 +1,84 @@
+﻿using AgendeMeWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendeMeWeb.Controllers
+{
+    public class PrefeituraController : Controller
+    {
+        // GET: PrefeituraController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PrefeituraController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PrefeituraController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PrefeituraController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PrefeituraController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PrefeituraController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PrefeituraController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PrefeituraController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Codigo/AgendeMeWeb/Models/PrefeituraViewModel.cs
+++ Codigo/AgendeMeWeb/Models/PrefeituraViewModel.cs
@@ -12,27 +12,27 @@ namespace AgendeMeWeb.Models
 
         [Required(ErrorMessage = "Campo requerido")]
         [StringLength(70, MinimumLength = 5, ErrorMessage = "Nome da prefeitura deve ter entre 5 e 70 caracteres")]
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
 
-        public string Cnpj { get; set; }
+        public string? Cnpj { get; set; }
 
         [Required(ErrorMessage = "Campo requerido")]
-        public string Estado { get; set; }
+        public string? Estado { get; set; }
 
         [Required(ErrorMessage = "Campo requerido")]
         [StringLength(30, MinimumLength = 5, ErrorMessage = "Nome da cidade deve ter entre 5 e 30 caracteres")]
-        public string Cidade { get; set; }
+        public string? Cidade { get; set; }
 
-        public string Bairro { get; set; }
+        public string? Bairro { get; set; }
 
-        public string Cep { get; set; }
+        public string? Cep { get; set; }
 
-        public string Rua { get; set; }
+        public string? Rua { get; set; }
 
         [Required(ErrorMessage = "Campo requerido")]
         [StringLength(7, MinimumLength = 1, ErrorMessage = "Número do edifício deve ter entre 1 e 7 caracteres")]
-        public string Numero { get; set; }
+        public string? Numero { get; set; }
 
-        public string Icone { get; set; }
+        public string? Icone { get; set; }
     }
 }
