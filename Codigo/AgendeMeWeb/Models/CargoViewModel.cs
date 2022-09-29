ed81ce2bd3194300caea5e1fd23f3604495b972c
Carlos Daniel <ms_danniel@outlook.com>
2022-09-29 16:19:58 -0300

    Criação e implementação da viewmodel e mapper do caso de uso cargo. #260

---
+++ Codigo/AgendeMeWeb/Models/CargoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendeMeWeb.Models
+{
+    public class CargoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
