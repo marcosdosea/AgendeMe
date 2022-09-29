63a6eaf90e4eb6318e22c19afc908de91043395c
Vinícius Carvalho <vini77carvalho77@gmail.com>
2022-09-28 21:18:12 -0300

    Implementação do caso de uso Manter Cidadão

---
+++ Codigo/AgendeMeWeb/Controllers/CidadaoController.cs
@@ -0,0 +1,94 @@
+﻿using AgendeMeWeb.Models;
+using AutoMapper;
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendeMeWeb.Controllers
+{
+    public class CidadaoController : Controller
+    {
+        private readonly ICidadaoService _cidadaoService;
+        private readonly IMapper _mapper;
+
+        public CidadaoController(ICidadaoService cidadaoService, IMapper mapper)
+        {
+            _cidadaoService = cidadaoService;
+            _mapper = mapper;
+        }
+
+        // GET: CidadaoController
+        public ActionResult Index()
+        {
+            var listaCidadaos = _cidadaoService.GetAll();
+            var listaCidadaosModel = _mapper.Map<List<CidadaoViewModel>>(listaCidadaos);
+            return View(listaCidadaosModel);
+        }
+
+        // GET: CidadaoController/Details/5
+        public ActionResult Details(int id)
+        {
+            Cidadao cidadao = _cidadaoService.Get(id);
+            CidadaoViewModel cidadaoViewModel = _mapper.Map<CidadaoViewModel>(cidadao);
+            return View(cidadaoViewModel);
+        }
+
+        // GET: CidadaoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CidadaoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CidadaoViewModel cidadaoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cidadao = _mapper.Map<Cidadao>(cidadaoViewModel);
+                _cidadaoService.Create(cidadao);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: CidadaoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Cidadao cidadao = _cidadaoService.Get(id);
+            CidadaoViewModel cidadaoViewModel = _mapper.Map<CidadaoViewModel>(cidadao);
+            return View(cidadaoViewModel);
+        }
+
+        // POST: CidadaoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, CidadaoViewModel cidadaoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cidadao = _mapper.Map<Cidadao>(cidadaoViewModel);
+                _cidadaoService.Edit(cidadao);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: CidadaoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Cidadao cidadao = _cidadaoService.Get(id);
+            CidadaoViewModel cidadaoViewModel = _mapper.Map<CidadaoViewModel>(cidadao);
+            return View(cidadaoViewModel);
+        }
+
+        // POST: CidadaoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, CidadaoViewModel cidadaoViewModel)
+        {
+            _cidadaoService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Codigo/Core/Service/ICidadaoService.cs
+++ Codigo/Core/Service/ICidadaoService.cs
@@ -2,5 +2,10 @@
 {
     public interface ICidadaoService
     {
+        int Create(Cidadao cidadao);
+        void Edit(Cidadao cidadao);
+        void Delete(int id);
+        Cidadao Get(int id);
+        IEnumerable<Cidadao> GetAll();
     }
 }
