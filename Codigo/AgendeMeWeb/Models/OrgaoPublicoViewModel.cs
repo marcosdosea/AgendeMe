95e9dcf9ddd85ef69291157262027823083f605b
Vinícius Carvalho <vini77carvalho77@gmail.com>
2022-09-30 12:24:02 -0300

    Criação do Model e Service

---
+++ Codigo/AgendeMeWeb/Models/OrgaoPublicoViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendeMeWeb.Models
+{
+    public class OrgaoPublicoViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatótio.")]
+        [StringLength(70, ErrorMessage = "O nome do órgão público deve ter no máximo 70 caracteres")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Bairro é obrigatótio.")]
+        [StringLength(70, ErrorMessage = "O bairro deve ter no máximo 70 caracteres")]
+        public string? Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo Rua é obrigatótio.")]
+        [StringLength(70, ErrorMessage = "A rua deve ter no máximo 70 caracteres")]
+        public string? Rua { get; set; }
+
+        [Display(Name = "Número")]
+        [Required(ErrorMessage = "O campo Número é obrigatório.")]
+        [StringLength(7, ErrorMessage = "O Número deve ter no máximo 7 caracteres")]
+        public string? Numero { get; set; }
+
+        [StringLength(70, ErrorMessage = "O Complemento deve ter no máximo 70 caracteres")]
+        public string? Complemento { get; set; }
+
+        [Display(Name = "CEP")]
+        [StringLength(10, ErrorMessage = "O CEP deve ter no máximo 10 caracteres")]
+        public string? Cep { get; set; }
+
+        [Display(Name = "Horário de abertura")]
+        [Required(ErrorMessage = "O campo Horário de abertura é obrigatório.")]
+        [StringLength(5, ErrorMessage = "O Horário de abertura deve ter no máximo 5 caracteres")]
+        public string? HoraAbre { get; set; }
+
+        [Display(Name = "Horário de fechamento")]
+        [Required(ErrorMessage = "O campo Horário de fechamento é obrigatório.")]
+        [StringLength(5, ErrorMessage = "O Horário de fechamento deve ter no máximo 5 caracteres")]
+        public string? HoraFecha { get; set; }
+        public int IdPrefeitura { get; set; }
+    }
+}
---
+++ Codigo/Service/OrgaoPublicoService.cs
@@ -0,0 +1,72 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class OrgaoPublicoService : IOrgaoPublicoService
+    {
+        private readonly AgendeMeContext _context;
+
+        public OrgaoPublicoService(AgendeMeContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Insere um novo órgão público na base de dados 
+        /// </summary>
+        /// <param name="orgaoPublico"></param>
+        /// <returns></returns>
+        public int Create(Orgaopublico orgaoPublico)
+        {
+            _context.Add(orgaoPublico);
+            _context.SaveChanges();
+            return orgaoPublico.Id;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(int id)
+        {
+            var _orgaoPublico = _context.Orgaopublicos.Find(id);
+            _context.Remove(_orgaoPublico);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orgaoPublico"></param>
+        public void Edit(Orgaopublico orgaoPublico)
+        {
+            _context.Update(orgaoPublico);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Orgaopublico Get(int id)
+        {
+            return _context.Orgaopublicos.Find(id);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Orgaopublico> GetAll()
+        {
+            return _context.Orgaopublicos.AsNoTracking();
+        }
+    }
+}
