549efeceb5cbf17e358a30a8851985f0c227370f
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-09-29 23:04:34 -0300

    Todos os arquivos essenciais para implementar o caso de uso foram criados, no entanto, ainda falta fazer testes para verificar a sua viabilidade. Sendo assim, ainda não será indicado gerar PR para esta branch. Done #245

--- Codigo/Core/Service/IAgendaDoServicoService.cs
+++ Codigo/Core/Service/IAgendaDoServicoService.cs
@@ -2,5 +2,11 @@
 {
     public interface IAgendaDoServicoService
     {
+        int Create(Agendadoservico agendadoservico);
+        void Edit(Agendadoservico agendadoservico);
+        void Delete(int id);
+        Agendadoservico Get(int id);
+        IEnumerable<Agendadoservico> GetAll();
+
     }
 }
---
+++ Codigo/Service/AgendaDoServicoService.cs
@@ -0,0 +1,51 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class AgendaDoServicoService : IAgendaDoServicoService
+    {
+        private readonly AgendeMeContext _context;
+
+        public AgendaDoServicoService(AgendeMeContext context)
+        {
+            _context = context;
+        }
+
+        public int Create(Agendadoservico agendaDoServico)
+        {
+            _context.Add(agendaDoServico);
+            _context.SaveChanges();
+            return agendaDoServico.Id;
+        }
+
+        public void Delete(int id)
+        {
+            var _agendaDoServico = _context.Agendamentos.Find(id);
+            _context.Remove(_agendaDoServico);
+            _context.SaveChanges();
+        }
+
+        public void Edit(Agendadoservico agendaDoServico)
+        {
+            _context.Update(agendaDoServico);
+            _context.SaveChanges();
+        }
+
+        public Agendadoservico Get(int id)
+        {
+            return _context.Agendadoservicos.Find(id);
+        }
+
+        public IEnumerable<Agendadoservico> GetAll()
+        {
+            return _context.Agendadoservicos.AsNoTracking();
+        }
+    }
+}
