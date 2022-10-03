6df26b0f39ce6b9018a6f551db59474c6eca4dea
Carlos Daniel de Jesus <ms_danniel@outlook.com>
2022-10-03 19:19:08 -0300

    Criada interface para profissional. #253

---
+++ Codigo/Core/Service/IProfissional.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IProfissional
+    {
+        int Create(Cidadao profissional);
+        void Edit(Cidadao profissional);
+        void Delete(int idProfissional);
+        Cidadao Get(int idProfissional);
+        IEnumerable<Cidadao> GetAll();
+    }
+}
