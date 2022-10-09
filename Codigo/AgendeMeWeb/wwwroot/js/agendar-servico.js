f7841bf155ccdca4745b3485e93b996cbdc4cf47
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-10-08 22:27:18 -0300

    Implementado tela 03_01

---
+++ Codigo/AgendeMeWeb/wwwroot/js/agendar-servico.js
@@ -0,0 +1,6 @@
+﻿function removeErro(select) {
+    if (select.value) {
+        var mensagemErro = document.getElementById("erroSelectPrefeitura");
+        mensagemErro.className = "d-none";
+    }
+}
