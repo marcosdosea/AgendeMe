fdf15ee31e899a8c3018c1f905f8b816b97c6f8d
Vinícius Carvalho <vini77carvalho77@gmail.com>
2022-10-12 19:48:48 -0300

    Criação do projeto "ServiceTests" e das classes de testes do service

---
+++ Codigo/ServiceTests/PrefeituraServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class PrefeituraServiceTests
+    {
+        [TestMethod()]
+        public void PrefeituraServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
