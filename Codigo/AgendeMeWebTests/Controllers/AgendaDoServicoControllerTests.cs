8063bb509738f2d0767b6b1cd3300b9ad40369d7
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-10-13 08:55:51 -0300

    Teste estão em construção. Além disso, o método DetailsTest está incompleto. Done #283

--- Codigo/AgendeMeWebTests/Controllers/AgendaDoServicoControllerTests.cs
+++ Codigo/AgendeMeWebTests/Controllers/AgendaDoServicoControllerTests.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AgendeMeWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace AgendeMeWeb.Controllers.Tests
 {
     [TestClass()]
     public class AgendaDoServicoControllerTests
     {
+        private static AgendaDoServicoController controller;
+        
         [TestMethod()]
         public void AgendaDoServicoControllerTest()
         {
@@ -20,13 +25,24 @@ namespace AgendeMeWeb.Controllers.Tests
         [TestMethod()]
         public void DetailsTest()
         {
-            Assert.Fail();
+            // Act
+            var result = controller.Details(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(AgendaDoServicoController));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(AgendaDoServicoController));  
+            AgendaDoServicoViewModel agendaDoServicoViewModel = (AgendaDoServicoViewModel)viewResult.ViewData.Model;
+            
         }
 
         [TestMethod()]
         public void CreateTest()
         {
-            Assert.Fail();
+            // Act
+            var result = controller.Create();
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
 
         [TestMethod()]
