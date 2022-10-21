f05ea86efeccdf4d56e3b9577d5dfbf94241a57b
Carlos Daniel <ms_danniel@outlook.com>
2022-10-21 11:45:01 -0300

    criado teste para Create do service de prefeitura. #318

--- Codigo/ServiceTests/PrefeituraServiceTests.cs
+++ Codigo/ServiceTests/PrefeituraServiceTests.cs
@@ -1,20 +1,64 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Service.Tests
 {
     [TestClass()]
     public class PrefeituraServiceTests
     {
-        [TestMethod()]
-        public void PrefeituraServiceTest()
+        private AgendeMeContext _context;
+        private IPrefeituraService _prefeituraService;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            Assert.Fail();
+            //Arrange
+            var builder = new DbContextOptionsBuilder<AgendeMeContext>();
+            builder.UseInMemoryDatabase("AgendeMe");
+            var options = builder.Options;
+
+            _context = new AgendeMeContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var prefeitura = new List<Prefeitura>
+                {
+                    new Prefeitura { Id = 1, Nome = "Prefeitura de Motorista", Cnpj = "94.259.898/0001-60", Estado = "SE", Cidade = "Ribeiropolis",
+                                             Bairro = "Centro", Cep = "49530-000", Rua = "Rua 1", Numero = "45", Icone = "dsfse" },
+
+                    new Prefeitura { Id = 2, Nome = "Prefeitura de Personal trainer", Cnpj = "90.796.680/0001-94",  Estado = "SE", Cidade = "Ribeiropolis",
+                                             Bairro = "Centro", Cep = "49530-000", Rua = "Rua 2", Numero = "65", Icone = "dsfse" },
+
+                    new Prefeitura { Id = 3, Nome = "Prefeitura de Marcos Dósea", Cnpj = "71.375.863/0001-91",  Estado = "SE", Cidade = "Ribeiropolis",
+                                             Bairro = "Centro", Cep = "49530-000", Rua = "Rua 3", Numero = "85", Icone = "dsfse"},
+
+                };
+
+            _context.AddRange(prefeitura);
+            _context.SaveChanges();
+
+            _prefeituraService = new PrefeituraService(_context);
         }
 
         [TestMethod()]
         public void CreateTest()
         {
-            Assert.Fail();
+            // Act
+            _prefeituraService.Create(new Prefeitura() { Id = 4, Nome = "Prefeitura de Eletricista", Cnpj = "71.375.863/0001-00", Estado = "BA", Cidade = "Salvador",
+                                                                 Bairro = "Baixa Norte", Cep = "49530-699", Rua = "Rua 4", Numero = "105", Icone = "HTTPS2168" });
+            // Assert
+            Assert.AreEqual(4, _prefeituraService.GetAll().Count());
+            var prefeitura = _prefeituraService.Get(4);
+            Assert.AreEqual("Prefeitura de Eletricista", prefeitura.Nome);
+            Assert.AreEqual("71.375.863/0001-00", prefeitura.Cnpj);
+            Assert.AreEqual("BA", prefeitura.Estado);
+            Assert.AreEqual("Salvador", prefeitura.Cidade);
+            Assert.AreEqual("Baixa Norte", prefeitura.Bairro);
+            Assert.AreEqual("49530-699", prefeitura.Cep);
+            Assert.AreEqual("Rua 4", prefeitura.Rua);
+            Assert.AreEqual("105", prefeitura.Numero);
+            Assert.AreEqual("HTTPS2168", prefeitura.Icone);
         }
 
         [TestMethod()]
