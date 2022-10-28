0fa20a7dd16ddccb68ed32f20c40e739d094a6aa
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-10-28 07:03:15 -0300

    Novas alterações relacionadas a estruturação da tela. Done #240

--- Codigo/AgendeMeWeb/Models/AgendaDoServicoViewModel.cs
+++ Codigo/AgendeMeWeb/Models/AgendaDoServicoViewModel.cs
@@ -6,17 +6,21 @@ namespace AgendeMeWeb.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Dia da semana")]
         [Required(ErrorMessage = "Campo requerido")]
         public string DiaSemana { get; set; }
 
 
+        [Display(Name = "Horário de início")]
         [Required(ErrorMessage = "Campo requerido")]
         [StringLength(5, MinimumLength = 1, ErrorMessage = "O Horário de fim deve ter entre 1 e 5 caracteres")]
         public string HorarioInicio { get; set; }
 
+        [Display(Name = "Horário de término")]
         [Required(ErrorMessage = "Campo requerido")]
         [StringLength(5, MinimumLength = 1, ErrorMessage = "O Horário de fim deve ter entre 1 e 5 caracteres")]
         public string HorarioFim { get; set; }
+
 
         [Required(ErrorMessage = "O número de VagasAtendimento é obrigatório")]
         public int VagasAtendimento { get; set; }
@@ -27,6 +31,7 @@ namespace AgendeMeWeb.Models
         [Required(ErrorMessage = "O número do serviço público é obrigatório")]
         public int IdServicoPublico { get; set; }
 
+        [Display(Name = "Nome do profissinal")]
         [Required(ErrorMessage = "O código do profissional é obrigatório")]
         public int IdProfissional { get; set; }
     }
