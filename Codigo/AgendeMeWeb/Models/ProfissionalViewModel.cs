0c51264f1f62053279ae59ea69786a9ed6b0a2f7
Carlos Daniel de Jesus <ms_danniel@outlook.com>
2022-10-28 21:49:47 -0300

    Adicionados campos requeridos e o display name. #331

--- Codigo/AgendeMeWeb/Models/ProfissionalViewModel.cs
+++ Codigo/AgendeMeWeb/Models/ProfissionalViewModel.cs
@@ -6,12 +6,23 @@ namespace AgendeMeWeb.Models
 {
     public class ProfissionalViewModel
     {
+        [Display(Name = "Cargo")]
+        [Required(ErrorMessage = "É obrigatório definir um cargo")]
         public int IdCargo { get; set; }
+
+        [Display(Name = "Profissional")]
+        [Required(ErrorMessage = "É obrigatório definir um profissional")]
         public int IdProfissional { get; set; }
+
+        [Display(Name = "Prefeitura")]
+        [Required(ErrorMessage = "É obrigatório definir uma prefeitura")]
         public int IdPrefeitura { get; set; }
 
+        [Display(Name = "Profissional")]
         public string? NomeProfissional { get; set; }
+        [Display(Name = "Prefeitura")]
         public string? NomePrefeitura { get; set; }
+        [Display(Name = "Cargo")]
         public string? NomeCargo { get; set; }
 
         public SelectList? ListaCargos { get; set; }
