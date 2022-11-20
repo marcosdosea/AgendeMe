f228b06119a9c87fdb42120d23d4cb584e61ee7a
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-11-20 10:12:41 -0300

    A classe ServicoPublicoServiceTests foi implementada. Done #319

--- Codigo/ServiceTests/ServicoPublicoServiceTests.cs
+++ Codigo/ServiceTests/ServicoPublicoServiceTests.cs
@@ -1,44 +1,103 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Service.Tests
 {
     [TestClass()]
     public class ServicoPublicoServiceTests
     {
-        [TestMethod()]
-        public void ServicoPublicoServiceTest()
+        private AgendeMeContext _context;
+        private IServicoPublicoService _autorService;
+
+        [TestInitialize()]
+        public void Initialize()
         {
-            Assert.Fail();
+            //Arrange
+            var builder = new DbContextOptionsBuilder<AgendeMeContext>();
+            builder.UseInMemoryDatabase("AgendeMe");
+            var options = builder.Options;
+
+            _context = new AgendeMeContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var servicosPublicos = new List<Servicopublico>
+                {
+                    new Servicopublico { Id = 1, Nome = "Médico", IdArea = 1, IdOrgaoPublico = 1, Icone = "Sem"},
+                    new Servicopublico { Id = 2, Nome = "Transporte coletivo", IdArea = 2, IdOrgaoPublico = 2, Icone = "Sem"},
+                    new Servicopublico { Id = 3, Nome = "Distribuição de medicamentos", IdArea = 1, IdOrgaoPublico = 1, Icone = "Sem"},
+                };
+
+            _context.AddRange(servicosPublicos);
+            _context.SaveChanges();
+
+            _autorService = new ServicoPublicoService(_context);
         }
 
         [TestMethod()]
         public void CreateTest()
         {
-            Assert.Fail();
+            // Act
+            _autorService.Create(new Servicopublico() { Id = 4, Nome = "Serviço funerário", IdArea = 3, IdOrgaoPublico = 3, Icone = "Sem" });
+            // Assert
+            Assert.AreEqual(4, _autorService.GetAll().Count());
+            var servicoPublico = _autorService.Get(4);
+            Assert.AreEqual("Serviço funerário", servicoPublico.Nome);
+            Assert.AreEqual(3, servicoPublico.IdArea);
+            Assert.AreEqual(3, servicoPublico.IdOrgaoPublico);
+            Assert.AreEqual("Sem", servicoPublico.Icone);
         }
 
         [TestMethod()]
         public void DeleteTest()
         {
-            Assert.Fail();
+            // Act
+            _autorService.Delete(2);
+            // Assert
+            Assert.AreEqual(2, _autorService.GetAll().Count());
+            var servicoPublico = _autorService.Get(3);
+            Assert.AreEqual("Distribuição de medicamentos", servicoPublico.Nome);
+            Assert.AreEqual(1, servicoPublico.IdArea);
+            Assert.AreEqual(1, servicoPublico.IdOrgaoPublico);
+            Assert.AreEqual("Sem", servicoPublico.Icone);
         }
 
         [TestMethod()]
         public void EditTest()
         {
-            Assert.Fail();
+            //Act 
+            var servicoPublico = _autorService.Get(1);
+            servicoPublico.Nome = "Serviços básicos de saúde";
+            _autorService.Edit(servicoPublico);
+            //Assert
+            servicoPublico = _autorService.Get(1);
+            Assert.IsNotNull(servicoPublico);
+            Assert.AreEqual("Serviços básicos de saúde", servicoPublico.Nome);
         }
 
         [TestMethod()]
         public void GetTest()
         {
-            Assert.Fail();
+            var servicoPublico = _autorService.Get(2);
+            Assert.IsNotNull(servicoPublico);
+            Assert.AreEqual("Transporte coletivo", servicoPublico.Nome);
+            Assert.AreEqual(2, servicoPublico.IdArea);
+            Assert.AreEqual(2, servicoPublico.IdOrgaoPublico);
+            Assert.AreEqual("Sem", servicoPublico.Icone);
         }
 
         [TestMethod()]
         public void GetAllTest()
         {
-            Assert.Fail();
+            // Act
+            var listaServicoPublico = _autorService.GetAll();
+            // Assert
+            Assert.IsInstanceOfType(listaServicoPublico, typeof(IEnumerable<Servicopublico>));
+            Assert.IsNotNull(listaServicoPublico);
+            Assert.AreEqual(3, listaServicoPublico.Count());
+            Assert.AreEqual(1, listaServicoPublico.First().Id);
+            Assert.AreEqual("Médico", listaServicoPublico.First().Nome);
         }
     }
 }
