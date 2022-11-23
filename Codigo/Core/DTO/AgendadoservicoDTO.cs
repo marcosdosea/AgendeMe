f608ea8916d41738f88b3e03931b7aac183f4bff
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-11-23 12:45:27 -0300

    Alterações. Done #240

---
+++ Codigo/Core/DTO/AgendadoservicoDTO.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.ComponentModel;
+
+namespace Core.DTO
+{
+    public class AgendadoservicoDTO 
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Dia")]
+        public string? DiaSemana { get; set; }
+        public string? HorarioInicio { get; set; }
+        public string? HorarioFim { get; set; }
+
+        [DisplayName("Agendamento")]
+        public int VagasAtendimento { get; set; }
+
+        [DisplayName("Retorno")]
+        public int VagasRetorno { get; set; }
+        
+        [DisplayName("Serviço")]
+        public string? NomeDoServicoPublico { get; set; }
+
+        [DisplayName("Profissional")]
+        public string? NomeDoProfissional { get; set; }
+    }
+}
