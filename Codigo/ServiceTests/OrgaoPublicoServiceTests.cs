340eeb8d10c2a8a46ab171b6f2a295df219c1048
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-03-02 04:36:18 -0300

    Teste ainda está incompleto

--- Codigo/ServiceTests/OrgaoPublicoServiceTests.cs
+++ Codigo/ServiceTests/OrgaoPublicoServiceTests.cs
@@ -1,17 +1,42 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Service.Tests
 {
     [TestClass()]
     public class OrgaoPublicoServiceTests
     {
-        [TestMethod()]
-        public void OrgaoPublicoServiceTest()
+        private AgendeMeContext _context;
+        private IOrgaoPublicoService _autorService;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            Assert.Fail();
+            //Arrange
+            var builder = new DbContextOptionsBuilder<AgendeMeContext>();
+            builder.UseInMemoryDatabase("AgendeMe");
+            var options = builder.Options;
+
+            _context = new AgendeMeContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var orgaosPublicos = new List<Orgaopublico>
+                {
+                    new Orgaopublico { Id = 1, Nome = "Posto de Saúde Souto Diniz", Bairro = "Centro", Rua = "Av. Otoniel Dória", Numero = "534", Complemento = "Centro da cidade", Cep = "49500-000", HoraAbre = "07:00", HoraFecha = "17:00", IdPrefeitura = 1},
+                    new Orgaopublico { },
+                    new Orgaopublico { }
+                };
+
+            _context.AddRange(orgaosPublicos);
+            _context.SaveChanges();
+
+            _orgaoPublicoService = new OrgaoPublicoService(_context);
         }
 
-        [TestMethod()]
+
+            [TestMethod()]
         public void CreateTest()
         {
             Assert.Fail();
