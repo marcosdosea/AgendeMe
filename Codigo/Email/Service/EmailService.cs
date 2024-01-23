d5c230caa95dff0191fa02a4edb2fc593c086b86
Carlos Daniel <ms_danniel@outlook.com>
2024-01-22 22:48:09 -0300

    service email #413

---
+++ Codigo/Email/Service/EmailService.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Email.Models;
+
+namespace Email
+{
+    public class EmailService
+    {
+        public static async Task<bool> Enviar(EmailModel mailModel)
+        {
+            SMTPModel smtpEmail = SMTPModel.Instance;
+
+            if (mailModel.To.Count == 0)
+            {
+                return false;
+            }
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress(mailModel.From, mailModel.Assunto);
+            foreach (var t in mailModel.To)
+            {
+                mail.To.Add(t);
+            }
+
+            mail.Subject = mailModel.Subject;
+
+            //centro do html do email
+            string bodyCenter = "<br><br><div style=\"padding-top: 20px;padding-bottom: 20px;background-color: #198754;\">" +
+                                    "<h2 style=\"color: white;text-align: center;\">" + mailModel.Assunto + "</h2>" +
+                                "</div>" +
+                                "<div style=\"color: black; text-align: left;\">" +
+                                    "<h3 style=\"text-indent: 10px;\">Olá, "/*+ mailModel.AddresseeName*/ + "</h3>" +
+                                    "<p style=\"text-indent: 10px; font-size:12pt;\">" +
+                                        mailModel.Body +
+                                    "</p>" +
+                                "</div><br>" +
+                                "<br><br>";
+
+            //mail.Body = mailModel.Body;
+            mail.Body = bodyCenter;
+
+            mail.IsBodyHtml = true;
+
+            using (SmtpClient smtp = new())
+            {
+                smtp.Host = mailModel.Host;
+                smtp.Credentials = new NetworkCredential(smtpEmail.UserName, smtpEmail.Password);
+                smtp.EnableSsl = true;
+                smtp.Port = smtpEmail.Port;
+
+
+                await smtp.SendMailAsync(mail);
+
+                return true;
+            }
+        }
+    }
+}
