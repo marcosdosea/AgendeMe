05a0cd832df43655e100ab53cc3d0d97b70e9642
Carlos Daniel <ms_danniel@outlook.com>
2024-01-22 22:47:43 -0300

    model para email #413

---
+++ Codigo/Email/Models/EmailModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Email
+{
+    public class EmailModel
+    {
+        public string From { get; set; } = "sistemabatala@gmail.com";
+        public string Assunto { get; set; } = string.Empty;
+        public List<string> To { get; set; } = new();
+        public string Subject { get; set; } = "Gestão Grupo Musical";
+        public string AddresseeName { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public string Host { get; set; } = "smtp.gmail.com";
+    }
+}
