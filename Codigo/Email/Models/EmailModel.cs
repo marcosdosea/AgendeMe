c3763ad3d12fd6bc4c76f2030db3681dbe0e9742
Carlos Daniel <ms_danniel@outlook.com>
2024-01-29 11:57:57 -0300

    correção das string #413

--- Codigo/Email/Models/EmailModel.cs
+++ Codigo/Email/Models/EmailModel.cs
@@ -2,10 +2,10 @@
 {
     public class EmailModel
     {
-        public string From { get; set; } = "sistemabatala@gmail.com";
+        public string From { get; set; } = "agendemeteste@gmail.com";
         public string Assunto { get; set; } = string.Empty;
         public List<string> To { get; set; } = new();
-        public string Subject { get; set; } = "Gestão Grupo Musical";
+        public string Subject { get; set; } = "Agende-Me";
         public string AddresseeName { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
         public string Host { get; set; } = "smtp.gmail.com";
