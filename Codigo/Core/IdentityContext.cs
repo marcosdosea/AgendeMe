bedee645cfed29750fd0578c7fc2711e11871689
Lauro Santana <devlauross@gmail.com>
2024-02-14 22:07:49 -0300

    Identity to Core

---
+++ Codigo/Core/IdentityContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core;
+
+public class IdentityContext : IdentityDbContext<UsuarioIdentity>
+{
+    public IdentityContext(DbContextOptions<IdentityContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ Codigo/Core/UsuarioIdentity.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Core;
+
+// Add profile data for application users by adding properties to the UsuarioIdentity class
+public class UsuarioIdentity : IdentityUser
+{
+}
